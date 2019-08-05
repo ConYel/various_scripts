2ccedfd6ad8e41349e2ab4900069a3a6996e5d82
ConYel <yeles.konstantinos@gmail.com>
2019-08-05 16:45:19 +0300

    add prerequisites to download_SRA.sh

--- download_SRA.sh
+++ download_SRA.sh
@@ -5,6 +5,24 @@
 
 INPUT_txt="$1"
 
+DOCKER="/usr/bin/docker"
+PIGZ="/usr/bin/pigz"
+
+if [ $# -lt 1 ]; then
+echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir"
+exit 1
+fi
+# check prerequisites
+PREREQS="${DOCKER} ${PIGZ}"
+
+for prereq in $PREREQS; do
+  if [ ! -x "${prereq}" ]; then
+    echo "the ${prereq} not found";
+    exit 1
+  fi
+done
+
+
 if [ $# -lt 1 ]; then
 echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir"
 exit 1
@@ -12,4 +30,5 @@ fi
 
 while IFS= read -r line; do
 docker run --rm  -v "$(pwd)":/data -w /data inutano/sra-toolkit  fasterq-dump "$line" -t /data/shm -e 12
+pigz --best "$line.fastq"
 done < "$INPUT_txt"
