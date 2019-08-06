8cc68b4ef47b678a20d5a970decdd4656d415366
ConYel <yeles.konstantinos@gmail.com>
2019-08-06 11:56:27 +0300

    changes to download_SRA.sh, cleaner pigz

--- download_SRA.sh
+++ download_SRA.sh
@@ -1,42 +1,42 @@
-#!/bin/bash
-# File: download_SRA.sh
-## Konstantinos Con_Yel
-## Download SRA data with the help of docker
-pt -s xpg_echo #for echo /n
+#!/bin/bash 
+# File: download_SRA.sh 
+## Konstantinos Con_Yel 
+## Download SRA data with the help of docker 
 
-INPUT_txt="$1"
+shopt -s xpg_echo
 
-DOCKER="/usr/bin/docker"
-PIGZ="/usr/bin/pigz"
+INPUT_txt="$1" 
 
-if [ $# -lt 1 ]; then
-echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir"
-exit 1
-fi
+DOCKER="/usr/bin/docker" 
+PIGZ="/usr/bin/pigz" 
+
+if [ $# -lt 1 ]; then 
+echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir" 
+exit 1 
+fi 
+
 # check prerequisites
-PREREQS="${DOCKER} ${PIGZ}"
+PREREQS="${DOCKER} ${PIGZ}" 
 
-for prereq in $PREREQS; do
-  if [ ! -x "${prereq}" ]; then
-    echo "the ${prereq} not found";
-    exit 1
-  fi
-done
-
-
-if [ $# -lt 1 ]; then
-echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir"
-exit 1
+for prereq in $PREREQS; do 
+if [ ! -x "${prereq}" ]; then 
+echo "the ${prereq} not found"; 
+exit 1 
 fi
+done 
 
+if [ $# -lt 1 ]; then 
+echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir" 
+exit 1
+ fi 
 
 while IFS= read -r line; do
- echo "Now downloading $line\n"
- docker run --rm -v "$(pwd)":/data -w /data inutano/sra-toolkit fasterq-dump "$line" -t /data/shm -e 12
- echo "Using pigz on $line.fastq"
- if [ -s "$line.fastq" ]; then
- pigz --best "$line.fastq";
- else
- pigz --best "$line_1.fastq";
- pigz --best "$line_2.fastq";
+ echo "Now downloading ${line}\n"
+ docker run --rm -v "$(pwd)":/data -w /data inutano/sra-toolkit fasterq-dump "${line}" -t /data/shm -e 12
+ if [ -s "${line}*.fastq" ]; then
+ echo "Using pigz on ${line}.fastq" 
+     pigz --best "${line}*.fastq";
+ else 
+    echo "${line}.fastq not found"
+ fi
  done < "$INPUT_txt"
