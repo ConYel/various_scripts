228e2e8265675c3052e79273d92447ace7f99e6e
ConYel <yeles.konstantinos@gmail.com>
2019-09-06 19:08:51 +0300

    add processors and pigz regex

--- download_SRA.sh
+++ download_SRA.sh
@@ -6,12 +6,13 @@
 shopt -s xpg_echo
 
 INPUT_txt="$1" 
+Processors="${2:-4}"
 
-DOCKER="/usr/bin/docker" 
-PIGZ="/usr/bin/pigz" 
+DOCKER="docker" 
+PIGZ="pigz" 
 
 if [ $# -lt 1 ]; then 
-echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir" 
+echo "USAGE: download_SRA.sh <INPUT_txt>  <Processors> file with all SRA ids one per line, downloads everything in working dir" 
 exit 1 
 fi 
 
@@ -19,23 +20,21 @@ fi
 PREREQS="${DOCKER} ${PIGZ}" 
 
 for prereq in $PREREQS; do 
-if [ ! -x "${prereq}" ]; then 
+if ! [ -x "$(command -v ${prereq})" ]; then 
 echo "the ${prereq} not found"; 
 exit 1 
 fi
 done 
 
-if [ $# -lt 1 ]; then 
-echo "USAGE: <INPUT_txt> file with all SRA ids one per line, downloads everything in working dir" 
-exit 1
- fi 
 
 while IFS= read -r line; do
- echo "Now downloading ${line}\n"
- docker run --rm -v "$(pwd)":/data -w /data inutano/sra-toolkit fasterq-dump "${line}" -t /data/shm -e 12
- if [ -s "${line}*.fastq" ]; then
+ echo "Now downloading "${line}"\n"
+ docker run --rm -v "$(pwd)":/data -w /data inutano/sra-toolkit fasterq-dump "${line}" -t /data/shm -e $Processors
+ if [[ -s ${line}_1.fastq ]]; then
  echo "Using pigz on ${line}.fastq" 
-     pigz --best "${line}*.fastq";
+     pigz --best ${line}*.fastq
+ elif [[ -s ${line}.fastq ]]; then 
+     pigz --best ${line}*.fastq
  else 
     echo "${line}.fastq not found"
  fi
