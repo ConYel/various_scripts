c4c9d3d7677ad1c5d0f2a5aacf6b9538e804283a
conyel <yeles.konstantinos@gmail.com>
2019-11-22 10:26:11 +0000

    added annotation stats

--- summary_samples_table.R
+++ summary_samples_table.R
@@ -2,8 +2,6 @@
 # creating summarizing table from SPAR resulted tables
 ## add todate
 todate <- format(Sys.time(), "%d_%b_%Y")
-
-dat_path <- str_glue("{my_exp}_analysis")
 ## import data and make dataframe
 library(tidyverse)
 path <- "/home/0/Project_piRNA/3_IPP_COLO205_SPAR_results"
@@ -25,29 +23,21 @@ general_stat <- c(
   "FASTQ reads:", "Total mapped reads:", "Uniquely-mapped reads:",
   "Multi-mapped reads:", "Total mapped reads:", "Reads [all]:"
 )
+annot_stat <- c(
+  "RNA_class", "Reads", "Peaks")
 
-<- read_tsv(annot_files[1], 
-            trim_ws = TRUE,
-         col_names = c("Summary", "Count"),
-         col_types = c(
-                  Summary = "c",
-                  Count = "i"
-                  ) 
-                ) %>% 
-  filter(Summary %in% general_stat) %>% 
-  mutate(Summary = if_else(Summary == "Reads [all]:",
-                           "Reads Final",Summary)) 
-
-my_sum_list <- map(mapstat_files, ~ read_tsv(.x,
+my_summary <- map(mapstat_files, ~ read_tsv(.x,
                               trim_ws = TRUE,
-                              col_names = c("Summary", "Counts"),
+                              col_names = c("Summary", "Counts", "percent"),
                               col_types = c(
                                 Summary = "c",
-                                Count = "i",  
+                                Count = "i", 
+                                percent = "n"
                               )) %>% 
   filter(Summary %in% general_stat) %>% 
   mutate(Summary = if_else(Summary == "Reads [all]:",
-                           "Reads Final", Summary))
+                           "Reads Final", Summary)) %>% 
+    select(-percent)
       ) %>% 
   bind_rows(.id = "samples") %>% 
   mutate(Summary = Summary %>% 
@@ -56,4 +46,17 @@ my_sum_list <- map(mapstat_files, ~ read_tsv(.x,
            str_replace_all("-","_")) %>% 
   pivot_wider(names_from = Summary, values_from = Counts)
 
+my_annot <- map(annot_files, ~ read_tsv(.x, 
+                                        trim_ws = TRUE) %>% 
+                  rename(RNA_class = "#RNA class") %>% 
+                  select(RNA_class, Reads) 
+                  ) %>% 
+  bind_rows(.id = "samples") %>% 
+  mutate(RNA_class = RNA_class %>% 
+           str_replace_all("-","_")) %>% 
+  pivot_wider(names_from = RNA_class, values_from = Reads)
+
+complete_table <- my_summary %>% 
+  inner_join(my_annot, by = "samples") %>% 
+  write_tsv(str_glue("summary_full_stat_{todate}.txt"))
 
